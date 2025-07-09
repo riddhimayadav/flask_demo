df719f2e5032e7cf58d5d66d1e318911165d7e53
Riddhima Yadav <riddhimayadav922@gmail.com>
2025-07-09 12:25:02 -0500

    data with ORMs

--- hello.py
+++ hello.py
@@ -1,9 +1,26 @@
 import git
 from flask import Flask, request, render_template, url_for, redirect, flash
 from forms import RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8294b5121bda5ca372871639977b8ceb'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 @app.route("/home")
@@ -15,6 +32,9 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
